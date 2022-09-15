30a373154140517c3f002853accb82166f5ba675
Travis Ramcharran <tc.ramcharran@gmail.com>
2022-09-14 22:50:33 -0500

    create, update, delete requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,7 +23,7 @@ router.get('/', (req, res) => {
         }
       }
     ]
-  }).then(dbProductData => res.json(dbProductData))
+  }).then(dbTagData => res.json(dbTagData))
   .catch(err => {
     console.log(err);
     res.status(500).json(err)
@@ -54,7 +54,7 @@ router.get('/:id', (req, res) => {
         }
       }
     ]
-  }).then(dbProductData => res.json(dbProductData))
+  }).then(dbTagData => res.json(dbTagData))
   
   .catch(err => {
     console.log(err);
@@ -65,14 +65,65 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    
+  .then(dbTagData => res.json(dbTagData))
+    
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+  
+  .then(dbTagData => {
+    if(!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
